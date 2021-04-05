59b2a12da668628e65602f6b71984b2df75e9abc
aminealamimejja <>
2021-04-05 22:27:42 +0000

    Le problème est résolue .

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,23 @@
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
-        // add your code here
+        {
+ 
+        
+        int i = 0, j = s.length() - 1;
+ 
+        
+        while (i < j) {
+ 
+           
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+ 
+            
+            
+            i++;
+            j--;
+        }
         return true;
-    }
-}
+        }}}
+
